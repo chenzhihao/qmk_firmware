e821e22958790fea3b426bc741e88bdd92d482fe
Zhihao Chen <zchen2@atlassian.com>
2022-11-19 22:34:28 +1100

    add link time optimisation

--- keyboards/lily58/keymaps/chenzhihao/rules.mk
+++ keyboards/lily58/keymaps/chenzhihao/rules.mk
@@ -8,6 +8,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+LTO_ENABLE = yes 		  # Enable link time optimization
 # OLED_ENABLE= yes     # OLED display
 OLED_ENABLE= no     # OLED display
 TAP_DANCE_ENABLE=yes
